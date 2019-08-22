d525d0c6e702bfaaaf358920c78dc2b60cb1139d
seanfaulkner <sean.faulkner@my.tccd.edu>
2019-08-21 19:03:05 -0500

    Initial version

---
+++ classwork/Section1/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * ITSE 1430
+ * Lab 1
+ * Sean Faulkner
+ */
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
