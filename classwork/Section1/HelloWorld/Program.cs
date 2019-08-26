612962375ac02a83756ee608432fb76f4a84dc1b
seanfaulkner <sean.faulkner@my.tccd.edu>
2019-08-26 18:56:04 -0500

    talked about variables and types

--- classwork/Section1/HelloWorld/Program.cs
+++ classwork/Section1/HelloWorld/Program.cs
@@ -3,16 +3,33 @@
  * Lab 1
  * Sean Faulkner
  */
-
 using System;
 
 namespace HelloWorld
 {
     class Program
     {
-        static void Main ( string[] args )
+        static void Main ( /*string[] args*/ )
         {
+            string name = "";
+
+            //string if = "";
+
+            //definitly assigned
+            //name = "Bob";
+            string name2 = Console.ReadLine ();
+            //name2 = Console.ReadLine ();
+
+            name2 = name = "Sue";
+            Console.WriteLine (name2);
             Console.WriteLine ("Hello World!");
+
+            int hours = 8;
+            double payRate = 15.25;
+
+            double totalPay = payRate * hours;
+
+            string fullName = "Fred" + " Jones";
         }
     }
 }
