44330a58c9c739a8d07b06d366bc4144c82a1e1b
seanfaulkner <sean.faulkner@my.tccd.edu>
2019-08-28 19:13:15 -0500

    added ability to rea movie

--- classwork/Section1/HelloWorld/Program.cs
+++ classwork/Section1/HelloWorld/Program.cs
@@ -11,6 +11,98 @@ namespace HelloWorld
     {
         static void Main ( /*string[] args*/ )
         {
+            // Movie data
+            string title;
+            int runLength;
+            int releaseYear;
+            string description;
+            bool haveSeen;
+
+            while (true)
+            {
+                char input = DisplayMenu ();
+                if (input == 'A')
+                    AddMovie ();
+                else if (input == 'Q')
+                    break;
+            };
+        }
+
+        static void AddMovie ()
+        {
+            // Get title
+            Console.Write ("Title: ");
+            string title = Console.ReadLine ();
+
+            // Get description
+            Console.Write ("Description: ");
+            string description = Console.ReadLine ();
+
+            // Get release year
+            int releaseYear = ReadInt32 ("Release Year: ");
+
+            // Get run length
+            int runLength = ReadInt32 ("Run length (in minutes): ");
+
+            // Get have seen
+            bool haveSeen = ReadBoolean ("Have Seen? ");
+        }
+
+        static bool ReadBoolean ( string message )
+        {
+            while (true)
+            {
+                Console.Write (message);
+
+                string input = Console.ReadLine ();
+
+                //int result = Int32.Parse (input);
+                bool result;
+                if (Boolean.TryParse (input, out result))
+                    return result;
+
+                Console.WriteLine ("Not a boolean");
+            };
+        }
+        static int ReadInt32 ( string message )
+        {
+            while (true)
+            {
+                Console.Write (message);
+
+                string input = Console.ReadLine ();
+
+                //int result = Int32.Parse (input);
+                //int result;
+                //if (Int32.TryParse (input, out result))
+                if(Int32.TryParse(input, out int result))
+                    return result;
+
+                Console.WriteLine ("Not a number");
+            };
+        }
+
+        static char DisplayMenu ()
+        {
+            do
+            {
+                Console.WriteLine ("A)dd Movie");
+                Console.WriteLine ("Q)uit");
+
+                string input = Console.ReadLine ();
+                if (input == "A" || input == "a")
+                {
+                    return 'A';
+                } else if (input == "Q" || input == "q")
+                {
+                    return 'Q';
+                } else
+                    Console.WriteLine ("Invalid input");
+
+            } while (true);
+        }
+        private static void DemoLanguage ()
+        {
             string name = "";
 
             //string if = "";
@@ -30,9 +122,7 @@ namespace HelloWorld
             double totalPay = payRate * hours;
 
             string fullName = "Fred" + " Jones";
-
-
-
         }
     }
 }
+
