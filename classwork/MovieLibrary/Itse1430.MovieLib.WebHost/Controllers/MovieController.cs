90c3a5158a3ca5887ad87cc3fb0bbc8ab99e296b
seanfaulkner <sean.faulkner@my.tccd.edu>
2019-12-02 18:26:46 -0600

    add edit delete

--- classwork/MovieLibrary/Itse1430.MovieLib.WebHost/Controllers/MovieController.cs
+++ classwork/MovieLibrary/Itse1430.MovieLib.WebHost/Controllers/MovieController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * ITSE 1430 
+ * 
+ * Handles movie requests.
+ */
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -9,6 +14,7 @@ using Itse1430.MovieLib.WebHost.Models;
 
 namespace Itse1430.MovieLib.WebHost.Controllers
 {
+    /// <summary>Handles requests for movies.</summary>
     public class MovieController : Controller
     {
         public MovieController ()
@@ -17,13 +23,110 @@ namespace Itse1430.MovieLib.WebHost.Controllers
             _database = new SqlMovieDatabase (connString.ConnectionString);
         }
 
-        // GET: Movie
+        /// <summary>Called to get the list of movies.</summary>        
         [HttpGet]
         public ActionResult Index ()
         {
-            var items = _database.GetAll ();
+            var items = _database.GetAll ()
+                                 .OrderBy (x => x.Title)
+                                 .ThenBy (x => x.ReleaseYear);
 
             var model = items.Select (i => i.ToModel ());
+
+            return View (model);
+        }
+
+        [HttpGet]
+        public ActionResult Create ()
+        {
+            return View ();
+        }
+
+        [HttpPost]
+        public ActionResult Create ( MovieModel model )
+        {
+           
+            try
+            {
+                //Validate
+                if (ModelState.IsValid)
+                {
+                    //Save if valid
+                    var movie = model.ToDomain ();
+                    _database.Add (movie);
+
+                    // PostRedirectGet
+                    return RedirectToAction ("Index");
+                };
+            } catch (Exception e)
+            {
+                // Don't use Exception overload - doesn't work
+                ModelState.AddModelError ("", e.Message);               
+            };
+
+            return View (model);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var movie = _database.Get (id);
+            if (movie == null)
+                return HttpNotFound ();
+
+            var model = movie.ToModel ();
+            return View (model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit ( int id )
+        {
+            var movie = _database.Get (id);
+            if (movie == null)
+                return HttpNotFound ();
+
+            var model = movie.ToModel ();
+            return View (model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit ( MovieModel model )
+        {
+            try
+            {
+                //Validate
+                if (ModelState.IsValid)
+                {
+                    //Save if valid
+                    var movie = model.ToDomain ();
+                    _database.Update (movie.Id, movie);
+
+                    //PRG 
+                    return RedirectToAction ("Edit", new { id = movie.Id });
+                };
+            } catch (Exception e)
+            {
+                //Don't use Exception overload - doesn't work
+                ModelState.AddModelError ("", e.Message);
+            };
+
+            return View (model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(MovieModel model)
+        {
+            try
+            {
+                _database.Remove (model.Id);
+
+                //PRG 
+                return RedirectToAction ("Index");
+            } catch (Exception e)
+            {
+                //Don't use Exception overload - doesn't work
+                ModelState.AddModelError ("", e.Message);
+            };
 
             return View (model);
         }
--- classwork/MovieLibrary/Itse1430.MovieLib.WebHost/Models/MovieModelExtensions.cs
+++ classwork/MovieLibrary/Itse1430.MovieLib.WebHost/Models/MovieModelExtensions.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿/*
+ * ITSE 1430 
+ * 
+ * Represents a movie instance.
+ */
+using System;
 
 namespace Itse1430.MovieLib.WebHost.Models
 {
+    /// <summary>Provides extension methods for <see cref="MovieModel"/>.</summary>
     public static class MovieModelExtensions
     {
-        public static MovieModel ToModel(this Movie source)
+        /// <summary>Converts a model to a movie.</summary>
+        /// <param name="source">The source.</param>
+        /// <returns>The movie.</returns>
+        public static Movie ToDomain ( this MovieModel source )
+        {
+            if (source == null)
+                return null;
+
+            return new Movie () {
+                Id = source.Id,
+                Title = source.Title,
+                Description = source.Description,
+                HasSeen = source.HasSeen,
+                Rating = source.Rating,
+                ReleaseYear = source.ReleaseYear,
+                RunLength = source.RunLength
+            };
+        }
+
+        /// <summary>Converts a movie to a model.</summary>
+        /// <param name="source">The source.</param>
+        /// <returns>The model.</returns>
+        public static MovieModel ToModel ( this Movie source )
         {
             if (source == null)
                 return null;
@@ -13,7 +41,7 @@ namespace Itse1430.MovieLib.WebHost.Models
                 Id = source.Id,
                 Title = source.Title,
                 Description = source.Description,
-                HasSeen  = source.HasSeen,
+                HasSeen = source.HasSeen,
                 Rating = source.Rating,
                 ReleaseYear = source.ReleaseYear,
                 RunLength = source.RunLength
